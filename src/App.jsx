817559438bf833f7b80e57220cb5a77a0b8b1f34
somraj112 <somrajnandi112@gmail.com>
2025-07-07 23:23:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
